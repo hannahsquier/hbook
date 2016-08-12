bc0385132f50a1260999e921303918d8952cc4d5
Hannah Squier <hannahsquier@gmail.com>
2016-08-11 17:26:42 -0700

    fix foreign key issue

--- db/migrate/20160811234034_change_author_id_to_user_id.rb
+++ db/migrate/20160811234034_change_author_id_to_user_id.rb
@@ -1,7 +1,7 @@
 class ChangeAuthorIdToUserId < ActiveRecord::Migration[5.0]
   def change
     remove_column :comments, :author_id
-    add_column :comments, :user_id, :references
+    add_column :comments, :user_id, :integer
       add_index :comments, :user_id
 
   end
