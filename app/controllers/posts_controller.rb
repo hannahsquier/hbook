4469da9325a7f1a04061bb4097739df1885cfb1d
Hannah Squier <hannahsquier@gmail.com>
2016-09-05 13:14:59 -0700

    fix post from wrong poster problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = User.find(params[:user_id]).posts.build(post_params)
+    @post = User.find(current_user.id).posts.build(post_params)
     @post.receiver_id = params[:user_id]
 
     if @post.save
       flash[:success] = "Thank you for your post."
       redirect_to user_posts_path(params[:user_id])
     else
-      
+
 
       errors = @post.errors.full_messages.join(", ")
       flash[:error] = "could not post #{errors}"
